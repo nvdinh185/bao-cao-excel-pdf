a53c258f0b58e2ac6f2e05d5ee3dba758cf2fb0f
nvdinh185 <nvdinh0905671805@gmail.com>
2019-07-02 11:06:51 +0700

    edit pdf5

--- pdf/create-pdf5.js
+++ pdf/create-pdf5.js
@@ -7,23 +7,23 @@ page_config = {
     size: 'A5',
     margin: 0,
     layout: 'landscape'
-},
+};
 
-    background = {
-        image: './images/LAWKI.jpg',
-        left: 0,
-        top: 0,
-        width: 595,
-        height: 420
-    },
+background = {
+    image: './images/LAWKI.jpg',
+    left: 0,
+    top: 0,
+    width: 595,
+    height: 420
+};
 
-    matrix = [
-        { x: 0, y: 100, value: "(0, 100)" },
-        { x: 20, y: 120, value: "(20, 120)" },
-        { x: 40, y: 140, value: "(40, 140)" },
-        { x: 60, y: 160, value: "(60, 160)" },
-        { x: 80, y: 180, value: "(80. 180)" }
-    ];
+matrix = [
+    { x: 0, y: 100, value: "(0, 100)" },
+    { x: 20, y: 120, value: "(20, 120)" },
+    { x: 40, y: 140, value: "(40, 140)" },
+    { x: 60, y: 160, value: "(60, 160)" },
+    { x: 80, y: 180, value: "(80. 180)" }
+];
 
 doc = new PDFDocument(
     page_config
