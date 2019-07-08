41db538ec493d1e9e191c0593c1fc874787c7f8a
nvdinh185 <nvdinh0905671805@gmail.com>
2019-07-08 16:35:51 +0700

    push to git by dinh

---
+++ excel/write-excel/exceljs-6-write.js
@@ -0,0 +1,22 @@
+var Excel = require('exceljs');
+var workbook = new Excel.Workbook();
+
+let writeFile = async () => {
+    try {
+        await workbook.xlsx.readFile('old.xlsx');
+        var worksheet = workbook.getWorksheet(2);
+        var row = worksheet.getRow(2);
+        row.getCell(1).value = 2;
+        row.getCell(2).value = 4;
+        workbook.xlsx.writeFile('new.xlsx');
+    } catch (err) {
+        console.log('Loi: ' + err)
+    }
+
+}
+
+writeFile().then(data => {
+    console.log('data')
+}).catch(err => {
+    console.log(err)
+})
