76101233eb28f05e150f921b5fda22e7e6091f27
nvdinh185 <nvdinh0905671805@gmail.com>
2019-07-08 09:53:31 +0700

    commit

--- excel/write-excel/exceljs-5-removeRow.js
+++ excel/write-excel/exceljs-5-removeRow.js
@@ -5,6 +5,15 @@ workbook.xlsx.readFile('old.xlsx')
     .then(function () {
         var worksheet = workbook.getWorksheet(1);
         let row = worksheet.getRow(2);
+        /* var i = 0, j = 0;
+        worksheet.eachRow(el => {
+            if (el._number >= 5) i++;
+        })
+        while (j < i) {
+            let row = worksheet.getRow(5);
+            worksheet.spliceRows(row._number, 1)
+            j++;
+        } */
         while (row._cells.length > 0) {
             worksheet.spliceRows(row._number, 1)
             row = worksheet.getRow(2);
